a2d76a886a0cd5872cbd59204ed2016a9790a02a
ThaliaR <thalia.rebecca@gmail.com>
2020-05-06 11:13:46 +0200

    added constructor for injection and deleted the yogacoach test

--- src/springtest/AnnotationDemoApp.java
+++ src/springtest/AnnotationDemoApp.java
@@ -12,12 +12,11 @@ public class AnnotationDemoApp {
 
         // get the bean from spring container (here the default bean id is used = classname)
         Coach theCoach = context.getBean("tennisCoach", Coach.class);
-        Coach theYogaCoach = context.getBean("yogaCoach", Coach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
 
-        System.out.println(theYogaCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
 
         // close the context
         context.close();
--- src/springtest/TennisCoach.java
+++ src/springtest/TennisCoach.java
@@ -1,11 +1,17 @@
 package springtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService theFortuneservice){
+        fortuneService = theFortuneservice;
+    }
 
     @Override
     public String getDailyWorkout() {
