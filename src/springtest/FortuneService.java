f422da47cb865a6cea4fbfdfe85daf99e62b9173
ThaliaR <thalia.rebecca@gmail.com>
2020-05-06 11:08:39 +0200

    added furtune and happyfortuneservice

---
+++ src/springtest/FortuneService.java
@@ -0,0 +1,6 @@
+package springtest;
+
+public interface FortuneService {
+
+    public String getFortune();
+}
--- src/springtest/TennisCoach.java
+++ src/springtest/TennisCoach.java
@@ -5,8 +5,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    private FortuneService fortuneService;
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
